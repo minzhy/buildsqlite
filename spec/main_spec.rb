240d8d6a5e8c211c7cd7ccec6a46dfb8d92baff3
minzhy <1324537990@qq.com>
2022-11-15 19:32:17 +0800

    part 4

---
+++ spec/main_spec.rb
@@ -0,0 +1,30 @@
+describe 'database' do
+    def run_script(commands)
+      raw_output = nil
+      IO.popen("./main", "r+") do |pipe|
+        commands.each do |command|
+          pipe.puts command
+        end
+
+        pipe.close_write
+
+        # Read entire output
+        raw_output = pipe.gets(nil)
+      end
+      raw_output.split("\n")
+    end
+
+    it 'inserts and retrieves a row' do
+      result = run_script([
+        "insert 1 user1 person1@example.com",
+        "select",
+        ".exit",
+      ])
+      expect(result).to match_array([
+        "db > Executed.",
+        "db > (1, user1, person1@example.com)",
+        "Executed.",
+        "db > ",
+      ])
+    end
+  end
--- util.h
+++ util.h
@@ -25,7 +25,7 @@ extern const uint32_t TABLE_MAX_ROWS;
 
 typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
 
-typedef enum { PREPARE_SUCCESS, PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { PREPARE_SUCCESS, PREPARE_NEGATIVE_ID, PREPARE_STRING_TOO_LONG, PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
 
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
@@ -43,8 +43,9 @@ typedef struct {
 
 typedef struct{
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE+1];
+  char email[COLUMN_EMAIL_SIZE+1];
+  // 这里需要+1，因为string的最后一位是null，如果要存COLUMN_EMAIL_SIZE位的char的话，要留一位下来存null
 } Row;
 
 typedef struct {
@@ -58,24 +59,29 @@ typedef struct {
   ssize_t input_length;
 } InputBuffer;
 
+// util.c
 void print_prompt();
 
+// main.c
 InputBuffer* new_input_buffer();
-
 MetaCommandResult do_meta_command(InputBuffer*);
-
 void read_input(InputBuffer* );
-
 void close_input_buffer(InputBuffer* );
 
+// statment.c
 PrepareResult prepare_statement(InputBuffer* ,Statement* );
-
 ExecuteResult execute_statement(Statement* ,Table*);
+ExecuteResult execute_select(Statement* statement, Table* table);
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement);
+ExecuteResult execute_insert(Statement* statement, Table* table);
+
+// row.c
+void print_row(Row* row);
+void* row_slot(Table* table, uint32_t row_num);
 void serialize_row(Row* source, void* destination);
 void deserialize_row(void* source, Row* destination);
-void* row_slot(Table* table, uint32_t row_num);
-ExecuteResult execute_select(Statement* statement, Table* table);
-void print_row(Row* row);
+
+// table.c
 Table* new_table();
 void free_table(Table* table);
 
